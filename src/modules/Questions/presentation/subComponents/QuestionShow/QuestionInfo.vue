b1e974a39cb75f24bbac69984b5ab7a9819d8447
Min3m <m.abdelmoneam.dev@gmail.com>
2026-05-18 11:43:34 +0300

    build: question info

--- src/modules/Questions/presentation/subComponents/QuestionShow/QuestionInfo.vue
+++ src/modules/Questions/presentation/subComponents/QuestionShow/QuestionInfo.vue
@@ -1,36 +1,23 @@
 <script setup lang="ts">
-  import EditIcon from '@/shared/icons/Privacy/EditIcon.vue';
+  import questionImage from '@/assets/images/question-image.png';
 </script>
 
 <template>
   <div class="question-info-wrapper">
-    <div class="question-show-info">
-      <h2 class="question-show-title">Show Question</h2>
+    <div class="question-info-categories">
+      <div v-for="item in 3" :key="item" class="question-info-item">
+        <label for="category">question type:</label>
+        <p>MsQ</p>
+      </div>
     </div>
     <div class="question-show-body">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut quas iste qui eum explicabo
-      repudiandae quam adipisci, amet voluptates architecto debitis! Mollitia, harum velit optio
-      expedita esse quis nisi explicabo.
-    </div>
-
-    <div class="question-actions">
-      <button class="btn btn-primary"><EditIcon /> {{ $t('edit') }}</button>
-      <button class="action-btn delete" title="Delete">
-        <svg
-          width="15"
-          height="15"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-        >
-          <path d="M3 6h18" />
-          <path d="M19 6v14c0 1-1 2-2 2H7c-1 0-2-1-2-2V6" />
-          <path d="M8 6V4c0-1 1-2 2-2h4c1 0 2 1 2 2v2" />
-        </svg>
-      </button>
+      <div class="question-body-text">
+        <label for="category">question </label>
+        <h2>What is the basic building block of a living organism?</h2>
+      </div>
+      <div class="question-body-img">
+        <img :src="questionImage" alt="" />
+      </div>
     </div>
   </div>
 </template>
