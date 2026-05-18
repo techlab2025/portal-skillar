dbf810ba966c82d3b0f559918e7a49e57dec4994
Min3m <m.abdelmoneam.dev@gmail.com>
2026-05-18 14:45:51 +0300

    build: question review & caution

--- src/modules/Questions/presentation/components/questionShow.vue
+++ src/modules/Questions/presentation/components/questionShow.vue
@@ -3,8 +3,9 @@
   import QuestionClarification from '../subComponents/QuestionShow/QuestionClarification.vue';
   import QuestionHeader from '../subComponents/QuestionShow/QuestionHeader.vue';
   import QuestionInfo from '../subComponents/QuestionShow/QuestionInfo.vue';
-import QuestionSolutionHint from '../subComponents/QuestionShow/QuestionSolutionHint.vue';
-import QuestionSolutionSteps from '../subComponents/QuestionShow/QuestionSolutionSteps.vue';
+  import QuestionReviewProcedures from '../subComponents/QuestionShow/QuestionReviewProcedures.vue';
+  import QuestionSolutionHint from '../subComponents/QuestionShow/QuestionSolutionHint.vue';
+  import QuestionSolutionSteps from '../subComponents/QuestionShow/QuestionSolutionSteps.vue';
 </script>
 
 <template>
@@ -17,6 +18,7 @@ import QuestionSolutionSteps from '../subComponents/QuestionShow/QuestionSolutio
       <QuestionSolutionHint />
       <QuestionSolutionSteps />
     </div>
+    <QuestionReviewProcedures />
   </div>
 </template>
 
@@ -27,10 +29,10 @@ import QuestionSolutionSteps from '../subComponents/QuestionShow/QuestionSolutio
     gap: 1rem;
     padding: 1rem;
   }
+
   .solution-container {
     display: grid;
     grid-template-columns: repeat(2, 1fr);
     gap: 1rem;
-    
   }
 </style>
---
+++ src/shared/icons/Question/AgreeIcon.vue
@@ -0,0 +1,18 @@
+<template>
+  <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path
+      d="M8.99927 16.502C13.1243 16.502 16.4993 13.127 16.4993 9.00195C16.4993 4.87695 13.1243 1.50195 8.99927 1.50195C4.87427 1.50195 1.49927 4.87695 1.49927 9.00195C1.49927 13.127 4.87427 16.502 8.99927 16.502Z"
+      stroke="white"
+      stroke-width="1.5"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+    <path
+      d="M5.81323 9.00141L7.93573 11.1239L12.1882 6.87891"
+      stroke="white"
+      stroke-width="1.5"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+  </svg>
+</template>
